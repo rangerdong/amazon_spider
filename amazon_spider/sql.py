a90fba6ed5d39b2e37f193e3f5e2756c650514d1
hbdong <844199574@qq.com>
2017-08-16 17:13:51 +0800

    修复 抓取详情逻辑， 若抓取详情时，没有profile 或者 有新评论时 更新profile数据 # scrapy 生命周期了解 启动时 与 结束时操作

--- amazon_spider/sql.py
+++ amazon_spider/sql.py
@@ -25,10 +25,10 @@ class ReviewSql(object):
               "`review_total`, `review_rate`, `pct_five`, `pct_four`, `pct_three`, " \
               "`pct_two`, `pct_one`, `latest_total`) " \
               "VALUES ('%s', %s, %s, %s, '%s', '%s', " \
-              "'%s', '%s', '%s', '%s', '%s', '%s', '%s')" %\
+              "'%s', '%s', '%s', '%s', '%s', '%s', 0)" %\
               (item['asin'], cls.conn.escape(item['product']), cls.conn.escape(item['brand']), cls.conn.escape(item['seller']), item['image'],
                item['review_total'], item['review_rate'], item['pct_five'], item['pct_four'],
-               item['pct_three'], item['pct_two'], item['pct_one'], item['review_total'])
+               item['pct_three'], item['pct_two'], item['pct_one'])
         try:
             if cls.check_exist_profile(item['asin']):
                 cls.update_profile_item(item)
@@ -45,11 +45,12 @@ class ReviewSql(object):
 
     @classmethod
     def update_profile_item(cls, item):
-        sql = "UPDATE `review_profile` SET `product`=%s, `brand`=%s, `seller`=%s, `review_total`='%s', `review_rate`='%s'," \
+        sql = "UPDATE `review_profile` SET `product`=%s, `brand`=%s, `seller`=%s, `image`=%s, `review_total`='%s', `review_rate`='%s'," \
               "`pct_five`='%s', `pct_four`='%s', `pct_three`='%s', `pct_two`='%s', `pct_one`='%s', `latest_total`=`review_total` " \
               "WHERE `asin`='%s'" % \
-              (cls.conn.escape(item['product']), cls.conn.escape(item['brand']), cls.conn.escape(item['seller']), item['review_total'], item['review_rate'],
-               item['pct_five'], item['pct_four'], item['pct_three'], item['pct_two'], item['pct_one'], item['asin'])
+              (cls.conn.escape(item['product']), cls.conn.escape(item['brand']), cls.conn.escape(item['seller']), item['image'],
+               item['review_total'], item['review_rate'],item['pct_five'], item['pct_four'], item['pct_three'], item['pct_two'],
+               item['pct_one'], item['asin'])
         try:
             cls.cursor.execute(sql)
             cls.conn.commit()
