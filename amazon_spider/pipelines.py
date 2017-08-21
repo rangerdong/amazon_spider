ca6603ea49493585fb1d13a4b2219b6b259c35b7
hbdong <844199574@qq.com>
2017-08-21 18:33:55 +0800

    优化 爬虫 cmd show

--- amazon_spider/pipelines.py
+++ amazon_spider/pipelines.py
@@ -30,7 +30,7 @@ class AmazonSpiderPipeline(object):
                 item['review_url'] = 'https://www.amazon.com' + item['review_url']
                 item['date'] = item_date.strftime('%Y-%m-%d')
                 ReviewSql.insert_detail_item(item)
-                print('save review detail--[asin]:', item['asin'], '[reviewID]:', item['review_id'])
+
                 return item
 
         if isinstance(item, SalesRankingItem):
